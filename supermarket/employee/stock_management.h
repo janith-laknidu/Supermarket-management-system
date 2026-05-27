ac5167aae400517ec4051fa11570c61fbd4a4f9b
Janith laknidu <nbuddhika156@gmail.com>
2026-05-27 21:21:27 +0530

    Update stock_management.h

--- supermarket/employee/stock_management.h
+++ supermarket/employee/stock_management.h
@@ -1,1 +1,13 @@
+#ifndef STOCK_MANAGEMENT_H
+#define STOCK_MANAGEMENT_H
+#include "product_management.h"
+
 void stockManagement();
+
+void addStock();
+void viewStock();
+void updateStock();
+void lowStockAlert();
+void removeDamagedStock();
+
+#endif 
