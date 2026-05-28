d6f22c5e549561173c5ba76331c1b7030fcd99f1
Janith laknidu <nbuddhika156@gmail.com>
2026-05-28 13:17:41 +0530

    Update reports.h

--- supermarket/employee/reports.h
+++ supermarket/employee/reports.h
@@ -1,1 +1,14 @@
-void reports();
+#ifndef REPORTS_H
+#define REPORTS_H       
+
+#include "product_management.h"
+
+void reportsMenu();
+
+void totalProductsReport();
+void totalStockReport();
+void inventoryValueReport();
+void lowStockReport();
+void productPriceReport();
+
+#endif /* REPORTS_H */
