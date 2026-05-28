4096d77df5f20bf9a29dd3580489db2807139168
Senehas <senehasdidulithaofc@gmail.com>
2026-05-28 14:10:01 +0530

    Add header guards and include product management in view_products.h

--- supermarket/customer/view_products.h
+++ supermarket/customer/view_products.h
@@ -1,1 +1,13 @@
+#ifndef VIEW_PRODUCTS_H
+#define VIEW_PRODUCTS_H
+
+#include "../employee/product_management.h"
+
 void viewProducts();
+void viewAllProducts();
+void viewAvailableProducts();
+void viewCategoryProducts();
+void viewNewProducts();
+void viewProductDetails();
+
+#endif // VIEW_PRODUCTS_H
