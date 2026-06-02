750da565f9a3d8c83aeb231afffbb18c81c4a4ed
Janith laknidu <nbuddhika156@gmail.com>
2026-06-02 22:28:13 +0530

    Add billing functions and header guard in billing.h

--- supermarket/customer/billing.h
+++ supermarket/customer/billing.h
@@ -1,1 +1,11 @@
-void processBilling();
+#ifndef BILLING_H
+#define BILLING_H
+
+void billingMenu();
+void addItemToBill();
+void viewBill();
+float calculateTotal();
+void printReceipt();
+void clearBill();
+
+#endif
